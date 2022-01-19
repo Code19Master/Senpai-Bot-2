13681a55fe9742fd1d5825dbbe76fb56172e7531
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-01-19 12:18:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 let Discord = require("discord.js");
-let client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+let client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, GUILD_MEMBERS, GUILD_BANS, GUILD_VOICE_STATES] });
 const { MessageEmbed } = require('discord.js');
 client.setMaxListeners(0);
 const moment = require("moment")
