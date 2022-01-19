a9fcb25f85afed02a50b0f58b9999bcf838b410f
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-01-19 12:12:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { FastType } = require('weky');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { DiscordTogether } = require('discord-together');
-const Auditlog = require("discord-auditlog");
+const { Auditlog } = require("discord-auditlog");
 
 
 
