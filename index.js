83def121642fd3800516166c52c8a4928351ba56
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-01-19 13:19:09 +0530

    init

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const { FastType } = require('weky');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { DiscordTogether } = require('discord-together');
-const Auditlog = require("discord-auditlog");
 
 
 
@@ -73,15 +72,72 @@ client.on('messageDelete', function(message, channel) {
 
 
 //audit logs
-Auditlog(client, {
-	"serverid": {
-		auditlog: "logs",	
-		movement: "logs",
-		auditmsg: true, // Default to fasle, recommend to set a channel
-		voice: true, // Set a Channel name if you want it
-		trackroles: true, // Default is False
-	}
+client.on("messageDelete", async (message) => {
+  const logs = message.guild.channels.cache.find(channel => channel.name === "logs");
+  if (message.guild.me.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS) && !logs) {
+    message.guild.channels.create("logs", { type: "GUILD_TEXT" });
+  }
+  if (!message.guild.me.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS) && !logs) { 
+    console.log("The logs channel does not exist and tried to create the channel but I am lacking permissions")
+  }  
+  const entry = await message.guild.fetchAuditLogs({ type: "MESSAGE_DELETE" }).then(audit => audit.entries.first())
+  let user = ""
+    if (entry.extra.channel.id === message.channel.id
+      && (entry.target.id === message.author.id)
+      && (entry.createdTimestamp > (Date.now() - 5000))
+      && (entry.extra.count >= 1)) {
+    user = entry.executor.username
+  }
+
+  else { 
+    user = message.author.username
+  }
+  let embed = new Discord.MessageEmbed()
+  .setTitle("Message was Deleted")
+  .setDescription(`A message was deleted in ${message.channel.name} by ${user}`)
+  .setColor("RANDOM")
+  .setTimestamp()
+  logs.send(({ embeds: [embed] }));
+
+  let channel = message.mentions.channels.first() || message.channel
+  let sniped = client.snipes.get(channel.id)
+  if(!sniped) {
+    message.channel.send(" :x: | There is nothing to snipe in " + channel.name)
+  } else {
+    let em = new Discord.MessageEmbed()
+    .setAuthor(sniped.author.tag, sniped.author.displayAvatarURL())
+    .setDescription(sniped.content)
+    .setColor("GREEN")
+    .setTimestamp()
+    if(sniped.image) {
+      em.setThumbnail(sniped.image)
+    }
+      logs.send({ embeds: [em] }); 
+  }
+})
+//Channel Create
+client.on('channelCreate', async channel => {
+  let embed = new Discord.MessageEmbed()
+  .setTitle("A Channel Was Created")
+  .setDescription("Channel with name: #" + channel.name + " was just created")
+  .setColor("RANDOM")
+  .setTimestamp()
+  
+    client.channels.cache.get('925362722298818601').send(({ embeds: [embed] }));
 });
+//Channel Delete
+client.on('channelDelete', async channel => {
+  let embed = new Discord.MessageEmbed()
+  .setTitle("A Channel Was Delete")
+  .setDescription("Channel with name: #" + channel.name + " was just Deleted")
+  .setColor("RANDOM")
+  .setTimestamp()
+  
+    client.channels.cache.get('925362722298818601').send(({ embeds: [embed] }));
+});
+
+
+
 
 
 
