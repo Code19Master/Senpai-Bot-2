6d827a4e2b3aa54666627e4fe65866fa90bd1248
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-01-19 11:49:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -117,39 +117,23 @@ client.on("messageDelete", async (message) => {
 })
 //Channel Create
 client.on('channelCreate', async channel => {
-  const logs = guild.channels.cache.find(channel => channel.name === "logs");
-  if (guild.me.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS) && !logs) {
-    guild.channels.create("logs", { type: "GUILD_TEXT" });
-  }
-  if (!guild.me.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS) && !logs) { 
-    console.log("The logs channel does not exist and tried to create the channel but I am lacking permissions")
-  }  
-
   let embed = new Discord.MessageEmbed()
   .setTitle("A Channel Was Created")
   .setDescription("Channel with name: #" + channel.name + " was just created")
   .setColor("RANDOM")
   .setTimestamp()
   
-    logs.send(({ embeds: [embed] }));
+    client.channels.cache.get('925362722298818601').send(({ embeds: [embed] }));
 });
 //Channel Delete
 client.on('channelDelete', async channel => {
-  const logs = guild.channels.cache.find(channel => channel.name === "logs");
-  if (guild.me.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS) && !logs) {
-    guild.channels.create("logs", { type: "GUILD_TEXT" });
-  }
-  if (!guild.me.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS) && !logs) { 
-    console.log("The logs channel does not exist and tried to create the channel but I am lacking permissions")
-  }  
-
   let embed = new Discord.MessageEmbed()
   .setTitle("A Channel Was Delete")
   .setDescription("Channel with name: #" + channel.name + " was just Deleted")
   .setColor("RANDOM")
   .setTimestamp()
   
-    logs.send(({ embeds: [embed] }));
+    client.channels.cache.get('925362722298818601').send(({ embeds: [embed] }));
 });
 
 
